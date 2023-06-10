473b12c845f88f85e233cf4324d477851468f0bf
donpdang <pddang@ucalgary.ca>
2023-06-10 13:58:31 -0600

    change server port, set proper response headers

--- react-admin-example/client/src/App.js
+++ react-admin-example/client/src/App.js
@@ -2,7 +2,7 @@ import { Admin, Resource } from "react-admin";
 import { UserList } from './components/User';
 import restProvider from 'ra-data-simple-rest';
 
-const dataProvider = restProvider('https://localhost:3000');
+const dataProvider = restProvider('http://localhost:3010');
 function App() {
   return (
       <Admin dataProvider={dataProvider}>
